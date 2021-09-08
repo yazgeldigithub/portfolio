2f34842da73fb08993f89b47c0b962bae86a9f89
yazgeldigithub <devster2016@gmail.com>
2021-09-08 02:55:54 -0500

    l

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,6 @@ import reduxIcon from "@iconify/icons-logos/redux";
 class About extends Component {
   render() {
     if (this.props.sharedBasicInfo) {
-      var profilepic = "images/" + this.props.sharedBasicInfo.image;
     }
     if (this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.about;
