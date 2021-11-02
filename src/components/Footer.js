e1d04a984de6e44b5407409a12a95759b8d612d7
yazgeldigithub <devster2016@gmail.com>
2021-11-02 18:02:10 -0500

    minor changes to projects section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -102,7 +102,7 @@ class Footer extends Component {
         <div className="col-md-12">
           <div className="social-links">
             {networks}{" "}
-            <a href="https://cin.ufpe.br/~fbma/Crack/Cracking%20the%20Coding%20Interview%20189%20Programming%20Questions%20and%20Solutions.pdf">
+            <a href="https://drive.google.com/file/d/1bRiZ1qcuPTJmdekBibG4bNHvDH1m3gnD/view?usp=sharing">
               <Button>Resume</Button>
             </a>
           </div>
